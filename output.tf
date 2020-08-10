57536a66bfc11a7d30c4f5227a7b03a606641a13
Rajesh Bavananatham <rajesh.bavan@gmail.com>
2020-08-11 09:15:17 +1000

    Fixed output to emit commands to test apis

--- output.tf
+++ output.tf
@@ -1,4 +1,35 @@
-output "loadbalancer-public-fqdn" {
-  description = "Public fqdn for the azure load balancer"
-  value       = azurerm_public_ip.loadbalancer-public-ip.fqdn
+output "weather_api_url" {
+  description = "Curl command to access weather API"
+  value       = "curl http://${azurerm_public_ip.loadbalancer-public-ip.fqdn}/weather?city=melbourne"
 }
+
+output "hello-nginxplus_api_url" {
+  description = "HTTPie command to access Hellow NGINX Plus API"
+  value       = "http ${azurerm_public_ip.loadbalancer-public-ip.fqdn}/hello-nginxplus-api"
+}
+
+output "f1_api_url" {
+  description = "HTTPie command to access F1 detail through API"
+  value       = "http ${azurerm_public_ip.loadbalancer-public-ip.fqdn}/f1-api/f1/drivers.json"
+}
+
+output "nginxplus_dashboard_url" {
+  description = "NGINX Plus dashboard App"
+  value       = "http://${azurerm_public_ip.loadbalancer-public-ip.fqdn}:8080/dashboard.html"
+}
+
+output "admin_api_url" {
+  description = "HTTPie command to access Admin API"
+  value       = "http ${azurerm_public_ip.loadbalancer-public-ip.fqdn}:8080/api/6/http/upstreams"
+}
+
+output "inventory_api_url" {
+  description = "HTTPie command to access Inventory API"
+  value       = "http ${azurerm_public_ip.loadbalancer-public-ip.fqdn}/warehouse-api/inventory"
+}
+
+output "pricing_api_url" {
+  description = "HTTPie command to access Pricing API"
+  value       = "http ${azurerm_public_ip.loadbalancer-public-ip.fqdn}/warehouse-api/pricing"
+}
+
